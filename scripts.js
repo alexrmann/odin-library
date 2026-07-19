f53426c6f8c84a1fac46e239b493f7e4dfcdbfe3
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-07-18 19:58:39 -0700

    Elaborate on base files

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function Book(title, author, pages, read) {
+  // SAFEGUARD
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
+  // PROPERTIES
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  // METHODS
+  this.info = function (readStatus) {
+    readStatus = this.read;
+    // console.log(readStatus);
+
+    let readLabel = (this.read == true) ? "read" : "not read yet";
+    // console.log(readLabel);
+    
+    console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + readLabel);
+  };
+}
