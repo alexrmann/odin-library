3a71e34a14361d34d00149b31734cfc2f67a00e4
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-07-20 00:05:45 -0700

    Add basic functionality for adding a book to the library array

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function Book(title, author, pages, read) {
     readStatus = this.read;
     // console.log(readStatus);
 
-    let readLabel = (this.read == true) ? "read" : "not read yet";
+    let readLabel = (this.read == true) ? "read" : "not read";
     // console.log(readLabel);
     
     // Long form of the above conditional (ternary) operator
@@ -31,6 +31,31 @@ function Book(title, author, pages, read) {
     //    readLabel = "not read yet";
     //  }
 
-    console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + readLabel);
+    console.log(this.title + " by " + this.author + " is " + this.pages + " pages long. I have " + readLabel + " this book. This book's unique identifier is " + this.id + ".");
   };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // Assign arguments to variables
+  // 
+  let bookTitle = title;
+  let bookAuthor = author;
+  let bookPages = pages;
+  let bookReadStatus = read;
+
+  // Create a book 
+  // 
+  // 1. Create a bookObject variable and call the Book Constructor with the previous argument variables
+  let bookObject = new Book(title, author, pages, read);
+
+  // 2. Assign unique id
+  let bookIdentifier = crypto.randomUUID();
+
+  // 3. Associate the identifier with the book object
+  bookObject.id = bookIdentifier;
+
+  // Store the book in the array
+  //
+  // 1. You'll need to push the `book` to the `myLibrary` array
+  myLibrary.push(bookObject);
 }
