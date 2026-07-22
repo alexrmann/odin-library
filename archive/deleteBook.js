897f33a65d65bf03b76ff1375f8c4479380f30ba
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-07-22 16:50:50 -0700

    Move original attempt at delete book script to archive

--- archive/deleteBook.js
+++ archive/deleteBook.js
@@ -3,23 +3,24 @@
 
 bookList.addEventListener("click", (e) => {
   let target = e.target;
+  console.log(target); // Let's check the target to debug
+  console.log(target.getAttribute("data-id")); // Let's check the target to debug
 
   // Consider encapsulating this is its own function to be used as a callback in the event listener.
   myLibrary.forEach((book) => {
-    if (target.id == book.id) {
-      // query the id (Question: can you just re-use the target?)
-      let bookNode = document.querySelector(`#${target.id}`)
+    // query the id (Question: can you just re-use the target?)
+    let bookNode = document.querySelector(`#${target.id}`);
+
+    if (bookNode.id === book.id) {
       // use .remove() to remove it from the DOM
-      bookNode.remove(); // or does target.remove() work?
+      bookNode.remove();
 
       // you also need to remove it from the array...
       // try finding the index of the matching object
       let index = myLibrary.indexOf(book);
       // then remove that item with the splice() method
-      myLibrary.splice(myLibrary[index], 1)
+      myLibrary.splice(myLibrary[index], 1);
       console.log(`Book ${book.id} removed.`);
-      break;
     }
   });
-   
 });
