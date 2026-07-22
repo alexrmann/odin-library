6b28679502cc63dbe1bcee2b5c01bba7c935bb45
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-07-21 19:10:47 -0700

    Add removal of the book node

--- deleteBook.js
+++ deleteBook.js
@@ -9,10 +9,12 @@ bookList.addEventListener('click', (e) => {
   // Consider encapsulating this is its own function to be used as a callback in the event listener.
   myLibrary.forEach((book) => {
     if (target.id == book.id) {
-      // query the id
+      // query the id (Question: can you just re-use the target?)
+      let bookNode = document.querySelector(`#${target.id}`)
       // use .remove() to remove it from the DOM
+      bookNode.remove(); // or does target.remove() work?
 
-      // you also need to remove it from the array
+      // you also need to remove it from the array...
       // try finding the index of the matching object
       let index = myLibrary.indexOf(book);
       // then remove that item with the splice() method
